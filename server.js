2b7dfde60e816f8d1e8be2e702707efb829dfbb9
agampa27 <157064333+agampa27@users.noreply.github.com>
2024-10-19 11:37:30 -0700

    profile attempt 3

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect('mongodb+srv://agampa:Deeper-Connections-Cal-Hacks@cluster0.ky94y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  /*useNewUrlParser: true,
+  useUnifiedTopology: true, */
+});
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Route for handling user signup
+app.get('/', (req, res) => {
+    res.sendFile(path.join('signIn.html'));
+  });
+
+app.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Hash the password before storing it
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // Create a new user document and save it to the database
+  const newUser = new User({
+    email: email,
+    password: hashedPassword
+  });
+
+  newUser.save((err) => {
+    if (err) {
+      res.status(500).send('Error registering new user. Please try again.');
+    } else {
+      res.send('User registered successfully!');
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
